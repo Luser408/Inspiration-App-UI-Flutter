c49e652beffb5c9d3bceaa757f097daa46765c1e
SP20-BSE-039 <hamza128482@gmail.com>
2024-08-20 12:14:07 +0500

    Inspiration App UI Welcome to the Inspiration App UI project! This Flutter app showcases an awesome and sleek user interface designed to provide a seamless user experience.  Features Modern UI Design: Clean and intuitive layout that enhances user engagement. Responsive Design: Optimized for both Android and iOS devices. Custom Widgets: Reusable and customizable widgets to speed up development. Getting Started To get started with the app:  Clone the repository. Install the required dependencies by running flutter pub get. Run the app using flutter run.
    Description
    The Inspiration App UI is a visually stunning Flutter application designed to inspire creativity and productivity. Whether you're looking to create a new project or refine an existing one, this app serves as a perfect example of how modern UI principles can be applied in Flutter. With a focus on clean design, smooth transitions, and intuitive navigation, the Inspiration App UI is built to impress.

---
+++ inspiration_app_ui/lib/main.dart
@@ -0,0 +1,170 @@
+import 'package:flutter/material.dart';
+
+void main()=> runApp(
+  MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ),
+);
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(244, 243, 243, 1),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        // brightness:Brightness.light,
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(Icons.menu,color: Colors.black87),
+          onPressed: () {  },
+        ),
+
+      ),
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.vertical(bottom: Radius.circular(30))
+              ),
+              padding:EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text('Find your', style: TextStyle(color: Colors.black87,fontSize: 25),),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text('Inspiration',style: TextStyle(color: Colors.black,fontSize: 40,fontWeight: FontWeight.bold),),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                        color: Color.fromRGBO(244, 243, 243, 1),
+                        borderRadius: BorderRadius.circular(15)
+                    ),
+
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        prefixIcon: Icon(Icons.search,color: Colors.black87,),
+                        hintText: "search you're looking for",
+                        hintStyle: TextStyle(color: Colors.grey,fontSize: 15),
+
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Promo Today',style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold),),
+                  SizedBox(height: 15,),
+                  Container(
+                      height: 200,
+                      child: ListView(
+                        scrollDirection: Axis.horizontal,
+                        children: <Widget>[
+                          promoCard('assets/images/two.jpg'),
+                          promoCard('assets/images/three.jpg'),
+                          promoCard('assets/images/one.jpg'),
+
+
+                        ],
+                      )
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    height: 200,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: AssetImage('assets/images/four.jpg'),
+                      ),
+                    ),
+                    child:Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          gradient: LinearGradient(
+                              begin: Alignment.bottomRight,
+                              stops: [0.1,0.9],
+                              colors: [
+                                Colors.black.withOpacity(.8),
+                                Colors.black.withOpacity(.0),
+                              ]
+                          )
+                      ),
+                      child: Align(
+                        alignment: Alignment.bottomLeft,
+                        child: Padding(
+                          padding: const EdgeInsets.all(15.0),
+                          child: Text('Best Design',style: TextStyle(color: Colors.white,fontSize: 20),),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+
+      ),
+    );
+  }
+  Widget promoCard(image){
+    return AspectRatio(
+      aspectRatio: 2.65 / 3,
+      child:Container(
+        margin: EdgeInsets.only(right: 15),
+        decoration: BoxDecoration(
+          color: Colors.orange,
+          borderRadius: BorderRadius.circular(20),
+          image: DecorationImage(
+              fit: BoxFit.cover,
+              image: AssetImage(image),
+          ),
+            ),
+        child:Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                gradient: LinearGradient(
+                    begin: Alignment.bottomRight,
+                    stops: [0.1,0.9],
+                    colors: [
+                      Colors.black.withOpacity(.8),
+                      Colors.black.withOpacity(.1),
+                    ]
+                )
+          ),
+        ),
+      ),
+    );
+  }
+}
